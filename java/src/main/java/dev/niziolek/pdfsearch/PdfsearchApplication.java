28fe3ebd81b025846e2557b548b9f8f4929ddddb
Norbert Niziołek <nizioleque@gmail.com>
2022-12-07 23:21:18 +0100

    first commit

---
+++ java/src/main/java/dev/niziolek/pdfsearch/PdfsearchApplication.java
@@ -0,0 +1,13 @@
+package dev.niziolek.pdfsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfsearchApplication.class, args);
+	}
+
+}
