f7a4f190f5c4b364db26d5b594613acbf68a0c2c
Norbert Niziołek <nizioleque@gmail.com>
2022-12-09 01:40:14 +0100

    Add PDF content reading

--- java/src/main/java/dev/niziolek/pdfsearch/PdfsearchApplication.java
+++ java/src/main/java/dev/niziolek/pdfsearch/PdfsearchApplication.java
@@ -1,13 +1,25 @@
 package dev.niziolek.pdfsearch;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+
 @SpringBootApplication
 public class PdfsearchApplication {
+  public static void main(String[] args) {
+    File pdfFile = new File("../pdfs/konstytucja.pdf");
+    try {
+      PDDocument doc = PDDocument.load(pdfFile);
+      String text = new PDFTextStripper().getText(doc);
+      System.out.println("Text in PDF: " + text.substring(0, 10000));
+    } catch (Exception ignored) {
+      System.out.println("Could not read PDF contents");
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(PdfsearchApplication.class, args);
-	}
+    SpringApplication.run(PdfsearchApplication.class, args);
+  }
 
 }
