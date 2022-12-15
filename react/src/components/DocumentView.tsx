38b4ced68508d7f10fab0a5945ba94018acfd65c
Norbert Niziołek <nizioleque@gmail.com>
2022-12-15 23:27:58 +0100

    Add document adding indication

--- react/src/components/DocumentView.tsx
+++ react/src/components/DocumentView.tsx
@@ -1,9 +1,12 @@
-import { Box, Button, Card, Typography } from '@mui/material';
+import { Box, Button, Card, CircularProgress, Typography } from '@mui/material';
 import { useDeleteDocumentMutation, useGetDocumentsQuery } from '../api';
+import { DocumentStatus } from '../types';
 import DocumentForm from './DocumentForm';
 
 function DocumentView() {
-  const { data: documents } = useGetDocumentsQuery();
+  const { data: documents } = useGetDocumentsQuery(undefined, {
+    pollingInterval: 1000,
+  });
   const [deleteDocument, {}] = useDeleteDocumentMutation();
 
   if (!documents) return <div>Loading...</div>;
@@ -24,12 +27,35 @@ function DocumentView() {
       <Box p={2}>
         <DocumentForm />
         {documents.map((document) => (
-          <Card variant='outlined' key={document.id}>
-            <Typography variant='h4'>{document.title}</Typography>
-            {document.author.length > 0 && (
-              <Typography>by {document.author}</Typography>
+          <Card
+            variant='outlined'
+            key={document.id}
+            sx={{
+              display: 'grid',
+            }}
+          >
+            <Box sx={{ gridRow: 1, gridColumn: 1 }}>
+              <Typography variant='h4'>{document.title}</Typography>
+              {document.author.length > 0 && (
+                <Typography>by {document.author}</Typography>
+              )}
+              <Button onClick={() => deleteDocument(document.id)}>
+                Remove
+              </Button>
+            </Box>
+            {document.status === DocumentStatus.ADDING && (
+              <Box
+                sx={{
+                  gridRow: 1,
+                  gridColumn: 1,
+                  zIndex: 1,
+                  background: 'rgb(255, 255, 255, 0.85)',
+                }}
+              >
+                <CircularProgress />
+                <Typography>Adding...</Typography>
+              </Box>
             )}
-            <Button onClick={() => deleteDocument(document.id)}>Remove</Button>
           </Card>
         ))}
       </Box>
--- react/src/types.ts
+++ react/src/types.ts
@@ -2,6 +2,12 @@ export interface Document {
   id: string;
   title: string;
   author: string;
+  status: DocumentStatus;
+}
+
+export enum DocumentStatus {
+  ADDING = 'ADDING',
+  ADDED = 'ADDED',
 }
 
 export interface SearchResult {
