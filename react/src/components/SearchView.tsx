b213b101448886f2a0dc4b741223a87ddc0dc848
Norbert Niziołek <nizioleque@gmail.com>
2022-12-15 01:34:42 +0100

    Fix incorrect request when query is empty

--- react/src/components/SearchView.tsx
+++ react/src/components/SearchView.tsx
@@ -1,14 +1,22 @@
+import { useState } from 'react';
 import { useGetDocumentsQuery, useLazyGetWordQuery } from '../api';
 
 function SearchView() {
   const { data: documents } = useGetDocumentsQuery();
   const [getWord, { data: searchResults }] = useLazyGetWordQuery();
+  const [showResult, setShowResults] = useState<boolean>(false);
 
   return (
     <>
       <h1>Search</h1>
-      <input type='search' onChange={(event) => getWord(event.target.value)} />
-      {searchResults && documents && (
+      <input
+        type='search'
+        onChange={(event) => {
+          setShowResults(event.target.value.length > 0);
+          if (event.target.value.length > 0) getWord(event.target.value);
+        }}
+      />
+      {searchResults && documents && showResult && (
         <>
           <h2>Results:</h2>
           {searchResults.map((searchResult) => (
